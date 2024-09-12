12ae3f076d498b820ba1c9178b8e2091b46a0c83
elias-avenger <eli0@outlook.com>
2024-09-12 17:34:35 +0300

    Added parish buttons and did styling for the page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -20,37 +20,92 @@ class _MainScreenState extends State<MainScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("User Data Example"),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('images/kijani_logo.png'),
+        ),
+        backgroundColor: const Color(0xff23566d),
+        title: Text(
+          "${ctrl.branchData['branch']}",
+          style: const TextStyle(color: Colors.white),
+        ),
         actions: [
-          ElevatedButton(
-            onPressed: () async {
-              if (await ctrl.logout()) {
-                Get.to(const LoginScreen());
-              }
-            },
-            child: Text(
-              "Logout",
-              style: TextStyle(color: Colors.orange[900]),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                if (await ctrl.logout()) {
+                  Get.to(const LoginScreen());
+                }
+              },
+              child: Text(
+                "Logout",
+                style: TextStyle(color: Colors.orange[900], fontSize: 16),
+              ),
             ),
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Display user data
-          Text("Branch: ${ctrl.branchData['branch']}"),
-          Text("PMC: ${ctrl.branchData['coordinator'].split(" | ")[1]}"),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Update the user data map
-              // userController.updateUserData('name', 'John Doe');
-              // userController.updateUserData('age', 30);
-            },
-            child: const Text('Update User Data'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Text(
+                  "PMC: ${ctrl.branchData['coordinator'].split(" | ")[1]}",
+                  style: const TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 20),
+                for (String parish in ctrl.branchData['parishes'])
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        style: const ButtonStyle(
+                          backgroundColor:
+                              WidgetStatePropertyAll(Color(0xff23566d)),
+                          shape: WidgetStatePropertyAll(
+                            RoundedRectangleBorder(
+                                borderRadius: BorderRadius.zero),
+                          ),
+                          minimumSize: WidgetStatePropertyAll(Size(300, 60)),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "${parish.split(' | ').last} Parish",
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                    ],
+                  ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.grey[400]),
+                    minimumSize: const WidgetStatePropertyAll(Size(200, 50)),
+                    maximumSize: const WidgetStatePropertyAll(Size(200, 50)),
+                  ),
+                  onPressed: () {},
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add, size: 24, color: Color(0xff23566d)),
+                      Text(
+                        'Add parish',
+                        style:
+                            TextStyle(color: Color(0xff23566d), fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
