cb1588ed7a84c08256c3236fe1d962b1a7082921
elias-avenger <eli0@outlook.com>
2024-09-24 22:52:55 +0300

    Added a function to remove some data from a locally stored dataset

--- lib/services/local_storage.dart
+++ lib/services/local_storage.dart
@@ -20,6 +20,23 @@ class LocalStorage {
     return data;
   }
 
+  Future<String> removeUnSyncedData({
+    required String type,
+    required String key,
+    required Map<String, dynamic> data,
+  }) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    var removed = await data.remove(key);
+    if (removed != null) {
+      data.isNotEmpty
+          ? await prefs.setString(type, jsonEncode(data))
+          : prefs.remove(type);
+      return 'removed';
+    } else {
+      return "failed";
+    }
+  }
+
   Future<bool> removeEverything() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.clear();
