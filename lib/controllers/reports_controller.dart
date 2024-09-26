22feb8bac6b97845b9371e235c99947b18583bd1
katoemma <emmanuelkato39@gmail.com>
2024-09-26 17:08:17 +0300

    added check network snackbar

--- lib/controllers/reports_controller.dart
+++ lib/controllers/reports_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kijani_pmc_app/services/aws.dart';
 import 'package:kijani_pmc_app/services/http_airtable.dart';
@@ -11,6 +12,12 @@ class ReportsController extends GetxController {
   AWSService awsAccess = AWSService();
   InternetCheck internetCheck = InternetCheck();
   var unSyncedReports = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    countUnSyncedReports();
+  }
 
   Future<String> submitReport({
     required Map<String, dynamic> reportData,
@@ -70,17 +77,30 @@ class ReportsController extends GetxController {
     return myPrefs.storeData(key: 'failedReports', data: storedReports);
   }
 
-  Future<int> countUnSyncedReports() async {
+  void countUnSyncedReports() async {
     Map<String, dynamic> reportsData =
         await myPrefs.getData(key: 'failedReports');
-    return reportsData.length;
+    unSyncedReports.value = reportsData.length;
   }
 
-  void uploadUnSyncedReports() async {
+  Future<void> uploadUnSyncedReports() async {
     Map<String, dynamic> reportsData =
         await myPrefs.getData(key: 'failedReports');
     String awsAccessMsg = await internetCheck.getAWSConMessage();
     String airtableAccessMsg = await internetCheck.getAirtableConMessage();
+
+    //show snackbar if not connected to internet
+    if (awsAccessMsg != "connected" || airtableAccessMsg != "connected") {
+      Get.snackbar(
+        "Unable to Sync",
+        "Please check your internet connection",
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        duration: const Duration(seconds: 5),
+      );
+      return;
+    }
+
     while (awsAccessMsg == "connected" &&
         airtableAccessMsg == "connected" &&
         reportsData.isNotEmpty) {
@@ -124,6 +144,6 @@ class ReportsController extends GetxController {
       awsAccessMsg = await internetCheck.getAWSConMessage();
       airtableAccessMsg = await internetCheck.getAirtableConMessage();
     }
-    unSyncedReports = (await countUnSyncedReports()) as RxInt;
+    countUnSyncedReports();
   }
 }
