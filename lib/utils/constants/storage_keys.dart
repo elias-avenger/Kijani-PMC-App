f5907f5d68311174f15bb023ef663af9a23dd779
katoemma <emmanuelkato39@gmail.com>
2025-03-23 19:08:19 +0300

    CREATE: created a file to manage local storage keys

---
+++ lib/utils/constants/storage_keys.dart
@@ -0,0 +1,6 @@
+String kUserDataKey = 'user_data';
+String kParishListDataKey = 'parish_list';
+String kGroupsDataKey = 'groups_list';
+
+String kFarmersDataKey = 'farmers_list';
+String kGardensDataKey = 'gardens_list';
