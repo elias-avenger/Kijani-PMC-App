07b8b495443d9f52e12e38c95dcadf7e5bfc2c96
katoemma <emmanuelkato39@gmail.com>
2025-03-23 19:07:49 +0300

    CREATE: created a file to manage Airtable constants

---
+++ lib/utils/constants/airtable_constants.dart
@@ -0,0 +1,19 @@
+import 'package:kijani_pgc_app/dotenv.dart';
+
+const String kAirtableApiKey = Env.kAirtableApiKey;
+
+//UG Operations
+const String kUgOperationsBaseId = "appcSe8EQxMbJSWfp";
+const String kPGCAssignmentTable = "PGC_Assignments";
+
+//UG Nursery Actions
+const String kUGNurseryActionsBaseId = "app9yul6FMnVUm7L4";
+const String kGroupsTable = "Groups";
+
+//Current UG Nursery Actions
+const String kCurrentNurseryBaseId = "appd8";
+
+//UG Gardens
+const String kUGGardensBaseId = "appJBOIeM2ZA5nhnV";
+const String kGardensTable = "Gardens";
+const String kFarmersTable = "Farmers";
