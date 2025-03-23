e978bc81d3f347b7c89979a0167b89838d2410c1
katoemma <emmanuelkato39@gmail.com>
2025-03-23 19:08:40 +0300

    CREATE: created a file to manage color constatnts

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color kfGreen = Color(0xFF3F51B5);
+  static const int primaryVariant = 0xFF303F9F;
+  static const int secondary = 0xFF03DAC6;
+}
